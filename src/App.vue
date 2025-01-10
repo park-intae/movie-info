fb7355de5438f3c6c1fc3a04d8acd2c672d19215
AzureAD\박인태 <pit1995@outlook.kr>
2025-01-10 15:05:41 +0900

    Event, Modal, Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,119 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <Event text="NEPLIX 강렬한 운명의 드라마, 경기크리쳐"/>
+  <h1>영화정보</h1>
+  <div v-for="(movie, i) in data" :key="i">
+    <figure>
+      <img :src="`${movie.imgUrl}`" :alt="movie.title" class="item"/>
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+      <p>개봉: {{ movie.year }}</p>
+      <p>장르: {{ movie.category }}</p>
+      <button @click="increaseLike(i)">좋아요</button
+      ><span>{{ movie.like }}</span>
+      <p>
+        <button @click="isModal=true; selectedMovie=i">상세보기</button>
+      </p>
+    </div>
+  </div>
+
+  <Modal
+    :data="data"
+    :isModal="isModal"
+    :selectedMovie="selectedMovie"
+    />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import data from './assets/movies.js';
+import Navbar from './components/Navbar.vue';
+import Modal from './components/Modal.vue';
+import Event from './components/Event.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      isModal: false,
+      data: data,
+      selectedMovie: 0,
+    };
+  },
+  methods: {
+    increaseLike(i) {
+      this.data[i].like++;
+    },
+  },
+  components:{
+    Navbar: Navbar,
+    Event: Event,
+    Modal: Modal,
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body {
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1, h2, h3 {
+  margin-bottom: 1rem;
+}
+
+p{
+  margin-bottom: 0.5rem;
+}
+
+button {
+  margin-right: 10px;
+  margin-bottom: 1rem;
+}
+
+.item {
+  width: 100%;
+  border: 1px solid #ccc;
+  display: flex;
+  margin-bottom: 20px;
+  padding: 1rem;
+}
+
+.item figure {
+  width: 30%;
+  margin-right: 1rem;
+}
+
+.item img {
+  width: 100%;
+}
+
+.item .info {
+  width: 100%;
+}
+
+.modal {
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.7);
+  align-items: center;
+}
+
+.modal .inner{
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
 }
 </style>
---
+++ src/assets/movies.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+      title: "노량",
+      year: 2023,
+      category: "액션, 드라마",
+      textRed: "color: red",
+      like: 0,
+      imgUrl: "./public/assets/노량.jpg",
+    },
+    {
+      title: "아쿠아맨과 로스트 킹덤",
+      year: 2024,
+      category: "액션, 판타지, 어드밴처",
+      like: 0,
+      imgUrl: "./public/assets/아쿠아맨.jpg",
+    },
+    {
+      title: "3월의 휴가",
+      year: 2023,
+      category: "애니메이션, 드라마",
+      like: 0,
+      imgUrl: "./public/assets/3월의휴가.jpg",
+    },
+  ]
+
+  export default data;
